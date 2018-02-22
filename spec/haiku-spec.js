17b184e9ccd5f70e51316ef1d00ca310cbf0006f
Sam Wellander <samwellander@gmail.com>
2018-02-22 13:13:00 -0800

    Add and configure karma

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -3,9 +3,7 @@
 describe('Haiku', function() {
     it ('should verify that the poem has three lines.', function() {
         const haiku = new haiku();
-
         expect(haiku.hasThreeLines()).toBe(true)
     })
-    ////questions: how can I intentionally structure specs?
 
 })
